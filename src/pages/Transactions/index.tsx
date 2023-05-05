bf4f625ca67a730920ef68d5ed9a49e0f753d73f
Guilherme Athayde <guiathayde303@gmail.com>
2023-05-05 14:52:23 -0300

    Add SearchForm component in Transactions page

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,13 +1,43 @@
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
+import { SearchForm } from './components/SearchForm';
 
-import { Container } from './styles';
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles';
 
 export function Transactions() {
   return (
-    <Container>
+    <>
       <Header />
       <Summary />
-    </Container>
+
+      <TransactionsContainer>
+        <SearchForm />
+
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight varient="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>01/05/2023</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <PriceHighlight varient="outcome">- R$ 59,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>04/05/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </>
   );
 }
