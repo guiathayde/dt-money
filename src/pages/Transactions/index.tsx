b14f90c7305f29b2089edab754f42c730846cef7
Guilherme Athayde <guiathayde303@gmail.com>
2023-05-05 14:08:23 -0300

    Add Header component and Transactions page

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from '../../components/Header';
+
+import { Container } from './styles';
+
+export function Transactions() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
